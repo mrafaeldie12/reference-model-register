5c39afcc4c611c9680b8645432fba473eee5b28a
Leonardo <leo_rovaris@hotmail.com>
2016-11-28 16:34:07 -0200

    combo categorias

--- public/js/areaProcesso/areaprocesso-controller.js
+++ public/js/areaProcesso/areaprocesso-controller.js
@@ -1,4 +1,4 @@
-referenceModelRegisterModule.controller('AreaProcessoController', ['$scope', '$http', 'AreaProcessoService', function($scope, $http, areaProcessoService) {
+referenceModelRegisterModule.controller('AreaProcessoController', ['$scope', '$http', 'AreaProcessoService', 'CategoriaService', function($scope, $http, areaProcessoService, categoriaService) {
     function getAllAreaProcessos() {
         areaProcessoService.getAll().then(function(response) {
             $scope.retrievedAreaProcessos = response.data;
@@ -6,6 +6,13 @@ referenceModelRegisterModule.controller('AreaProcessoController', ['$scope', '$h
             console.log('Error');
         });
     };
+
+    categoriaService.getAll().then(function(response) {
+        $scope.categorias = response.data;
+        }, function() {
+            console.log('Error');
+    });
+
 
     $scope.retrievedAreaProcessos = [];
     $scope.areaProcesso = {};
