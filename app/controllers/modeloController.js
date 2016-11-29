817450a42e439ac7877ea6c84d5c19b3ce7c09dc
Rafael Dietrich <mrafael.dietrich@gmail.com>
2016-11-29 17:17:36 -0200

    feat: Finished form

--- app/controllers/modeloController.js
+++ app/controllers/modeloController.js
@@ -118,28 +118,83 @@ router.get('/form/:id', function(req, res, next) {
                 res.end();
             });
 
-            pdfDocument.fontSize(26)
+            pdfDocument.fontSize(22)
             .text(existingModelo.nome)
-            .fontSize(24)
+            .fontSize(20)
             .text('\nInformações:')
-            .fontSize(22)
-            .text(existingModelo.sigla)
-            .text(existingModelo.descricao)
-            .text('\nÁreas de Processo:');
-            for(var index=0; index < existingModelo._areaProcesso.length; index++)
-            {
-                var areaProcesso = existingModelo._areaProcesso[index];
-                pdfDocument.text('Descrição: ' + areaProcesso.sigla)
-                .text('Nome: ' + areaProcesso.nome)
-                .text('Descrição: ' + areaProcesso.descricao)
+            .fontSize(18)
+            .text('Sigla: ' + existingModelo.sigla)
+            .text('Descrição: ' + existingModelo.descricao)
+            .text('\n');
+            if(existingModelo._areaProcesso){
+                pdfDocument.fontSize(20)
+                .text('Áreas de Processo:')
+                .text('\n');
+                for(var index=0; index < existingModelo._areaProcesso.length; index++) {
+                    var areaProcesso = existingModelo._areaProcesso[index];
+
+                    var categoria = areaProcesso._categoria ? areaProcesso._categoria.nome : 'Sem Categoria';
+                    var nivelMaturidade = areaProcesso._nivelMaturidade ? areaProcesso._nivelMaturidade.nome : 'Sem Nível de Maturidade';
+
+                    pdfDocument.fontSize(18)
+                    .text('Área de Processo ' + index)
+                    .fontSize(16)
+                    .text('Sigla: ' + areaProcesso.sigla)
+                    .text('Nome: ' + areaProcesso.nome)
+                    .text('Descrição: ' + areaProcesso.descricao)
+                    .text('Categoria de Processo: ' + categoria)
+                    .text('Nível de Maturidade: ' + nivelMaturidade)
+                    .text('\n');
+
+                    if(areaProcesso._metaEspecifica && areaProcesso._metaEspecifica.length > 0){
+                        pdfDocument.fontSize(20)
+                        .text('Metas Específicas:')
+                        .text('\n');
+                        for(var metaIndex =0; metaIndex < areaProcesso._metaEspecifica.length; metaIndex++) {
+                            var metaEspecifica = areaProcesso._metaEspecifica[metaIndex];
+
+                            pdfDocument.fontSize(18)
+                            .text('Meta Específica ' + metaIndex)
+                            .fontSize(16)
+                            .text('Sigla: ' + metaEspecifica.sigla)
+                            .text('Nome: ' + metaEspecifica.nome)
+                            .text('Descrição: ' + metaEspecifica.descricao)
+                            .text('\n');
+                            
+                            if(metaEspecifica._praticaEspecifica && metaEspecifica._praticaEspecifica.length > 0){
+                                pdfDocument.fontSize(20)
+                                .text('Práticas Específicas:\n')
+                                .text('\n');
+                                for(var praticaIndex = 0; praticaIndex < metaEspecifica._praticaEspecifica.length; praticaIndex++){
+                                    var praticaEspecifica = metaEspecifica._praticaEspecifica[praticaIndex];
+
+                                    pdfDocument.fontSize(18)
+                                    .text('Prática Específica ' + metaIndex)
+                                    .fontSize(16)
+                                    .text('Sigla: ' + praticaEspecifica.sigla)
+                                    .text('Nome: ' + praticaEspecifica.nome)
+                                    .text('Descrição: ' + praticaEspecifica.descricao)
+                                    .text('\n');
+                                }
+                            } else {
+                                pdfDocument.fontSize(20)
+                                .text('Sem Práticas Específicas')
+                                .text('\n');
+                            }
+                        }
+                    } else {
+                        pdfDocument.fontSize(20)
+                        .text('Sem Metas Específicas')
+                        .text('\n');
+                    }
+                }
+            } else {
+                pdfDocument.fontSize(20)
+                .text('Sem Áreas de Processo')
                 .text('\n');
             }
 
-            pdfDocument.text(existingModelo.descricao);
-
             pdfDocument.end();
-            
-            //res.download(pdfFileName);
         }
     });
 });
